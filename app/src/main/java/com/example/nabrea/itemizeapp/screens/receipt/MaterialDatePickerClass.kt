572ae752e66e5c23f10be84f9d0f3aa788886406
nmabrea <nmabrea@gmail.com>
2020-09-18 15:18:33 -0700

    New custom class to replace the old MaterialDatePickerInterface

---
+++ app/src/main/java/com/example/nabrea/itemizeapp/screens/receipt/MaterialDatePickerClass.kt
@@ -0,0 +1,41 @@
+package com.example.nabrea.itemizeapp.screens.receipt
+
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+
+class MaterialDatePickerClass(
+    private val editText: TextInputEditText,
+    private val fragManager: FragmentManager) {
+
+    private val builder = MaterialDatePicker.Builder.datePicker()
+
+    private val picker = builder.build()
+
+    fun setTitle() { builder.setTitleText("SELECT A DATE") }
+
+    fun clickActions(listener: ReceiptFragmentCommunication) {
+        editText.setOnClickListener { editTextView ->
+
+            picker.show(fragManager, picker.toString())
+
+        }
+
+        picker.addOnPositiveButtonClickListener { dateSelected ->
+
+            editText.setText("${picker.headerText}")
+
+            listener.displaySnackbar("Date Selected: ${editText.text}.")
+
+        }
+
+        picker.addOnCancelListener { cancelled ->
+            listener.displaySnackbar("Calendar closed.")
+        }
+
+        picker.addOnNegativeButtonClickListener { cancelled ->
+            listener.displaySnackbar("Calendar closed.")
+        }
+
+    }
+}
