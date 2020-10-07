c4255dd00d37994b52519154d9ac7970c0f3c23e
nmabrea <nmabrea@gmail.com>
2020-10-06 18:31:54 -0700

    Updated gradle versions. Created a custom dialog to update Room entity.

---
+++ app/src/main/java/com/example/nabrea/itemizeapp/screens/receipt/expense/UpdateExpenseDialogFragment.kt
@@ -0,0 +1,59 @@
+package com.example.nabrea.itemizeapp.screens.receipt.expense
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.*
+import androidx.core.content.res.ResourcesCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.nabrea.itemizeapp.R
+import com.example.nabrea.itemizeapp.activity.ItemizeViewModel
+import com.example.nabrea.itemizeapp.databinding.FragmentDialogUpdateExpenseBinding
+
+class UpdateExpenseDialogFragment : DialogFragment() {
+
+    private lateinit var updateExpenseBinding: FragmentDialogUpdateExpenseBinding
+
+    private lateinit var updateExpenseVm: ItemizeViewModel
+
+    private lateinit var updateDialog: Dialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        updateExpenseBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_dialog_update_expense,
+            container,
+            false
+        )
+
+        updateExpenseVm = ViewModelProvider(activity!!).get(ItemizeViewModel::class.java)
+
+        updateExpenseBinding.itemizeViewModel = updateExpenseVm
+
+        return updateExpenseBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        updateDialog.window?.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        updateDialog = super.onCreateDialog(savedInstanceState)
+
+        updateDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        updateDialog.window?.setBackgroundDrawable(ResourcesCompat.getDrawable(resources, R.drawable.itm_button_add, null))
+
+        return updateDialog
+    }
+}
